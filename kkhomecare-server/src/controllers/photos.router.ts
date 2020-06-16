e37a53bbae2cc144f1ef0dd73f0f5791797da57d
Kacy Sommers <kacy@dev.adaptiva.com>
2020-06-15 22:15:24 -0700

    photos stuff

--- kkhomecare-server/src/controllers/photos.router.ts
+++ kkhomecare-server/src/controllers/photos.router.ts
@@ -29,106 +29,16 @@ router.get('', (req: Request, res: Response) => {
     .catch(error => res.status(500).json({ error }));
 });
 
-const getBeforeAfter = (fetchAll: boolean, getTotal: boolean, offset: number) => {
-  return new Promise((resolve, reject) => {
-    if (fetchAll) {
-      BeforeAfterModel
-        .countDocuments()
-        .exec()
-        .then(total => {
-          BeforeAfterModel
-            .find()
-            .exec()
-            .then(images => resolve({ total, images, success: true }))
-            .catch(err => reject({ error: err, success: false }))
-        }).catch(err => reject({ error: err, success: false }))
-    } else {
-      if (getTotal) {
-        BeforeAfterModel
-          .countDocuments()
-          .exec()
-          .then(total => {
-            BeforeAfterModel
-              .find()
-              .skip(offset)
-              .limit(1)
-              .exec()
-              .then(images => resolve({ total, images, success: true }))
-              .catch(err => reject({ error: err, success: false }))
-          }).catch(err => reject({ error: err, success: false }))
-      } else {
-        BeforeAfterModel
-          .find()
-          .skip(offset)
-          .limit(1)
-          .exec()
-          .then(images => resolve({ images, success: true }))
-          .catch(err => reject({ error: err, success: false }))
-      }
-    }
-  });
-};
-
-const getPhotos = (fetchAll: boolean, getTotal: boolean, lastId: number, limit: number) => {
-  return new Promise((resolve, reject) => {
-    if (fetchAll) {
-      ImageModel
-        .countDocuments()
-        .exec()
-        .then(total => {
-          ImageModel
-            .find()
-            .exec()
-            .then(images => resolve({ images, total, success: true }))
-            .catch(err => reject({ error: err, success: false }))
-        })
-        .catch(err => reject(err))
-    } else {
-      const query = lastId ? { _id: { $gt: lastId } } : {};
-      if (getTotal) {
-        ImageModel
-          .countDocuments()
-          .exec()
-          .then(total => {
-            ImageModel
-              .find(query)
-              .limit(limit)
-              .exec()
-              .then(images => resolve({ total, images, success: true }))
-              .catch(err => reject({ error: err, success: false }))
-          })
-          .catch(err => reject(err))
-      } else {
-        ImageModel
-          .find(query)
-          .limit(limit)
-          .exec()
-          .then(images => resolve({ images, success: true }))
-          .catch(err => reject({ error: err, success: false }))
-      }
-    }
-  });
-}
-
-router.post('/', (req: Request, res: Response) => {
-  const {
-    limit,
-    offset,
-    lastId,
-    getTotal,
-    fetchAll,
-    beforeAfter
-  } = req.body;
-  if (beforeAfter) {
-    getBeforeAfter(fetchAll, getTotal, offset)
-      .then(result => res.json(result))
-      .catch(error => res.sendStatus(500).json(error))
-  } else {
-    getPhotos(fetchAll, getTotal, lastId, limit)
-      .then(result => res.json(result))
-      .catch(error => res.sendStatus(500).json(error))
-  }
+router.get('/before-after', (req: Request, res: Response) => {
+  BeforeAfterModel
+    .find()
+    .exec()
+    .then(images => {
+      res.status(200).json({ images });
+    })
+    .catch(error => res.status(500).json({ error }));
 });
+
 
 router.post('/before-after/upload', upload.array('photos', 2), (req: Request, res: Response) => {
 
